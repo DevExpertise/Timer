d09ad68372ff0f45475966e40b49472880d3fb19
Souptik Roy <155081347+DevExpertise@users.noreply.github.com>
2024-04-14 20:21:47 +0530

    Update time_countdown.py

--- time_countdown.py
+++ time_countdown.py
@@ -1,8 +1,16 @@
+
 import time#import time module
 print("Set timer.")
-my_time=int(input("Enter time in second:"))#take time in second
+my_time_h=int(input("Enter  Hour:"))#take time in hour
+my_time_h=3600*my_time_h#hour in second
+
+my_time_m=int(input("Enter  Minute:"))#take time in minute
+my_time_m=60*my_time_m#convert in second
+my_time_s=int(input("Enter  second:"))#take time in second
+#Sum of  all time in second
+my_time=my_time_m+my_time_s+my_time_h
 for i in range(my_time,0,-1):
-#calculate second ,minute and hour
+#calculate second, minute and hour
     seconds=i%60
     minutes=i//60
     hours=int(i/3600)
@@ -11,3 +19,4 @@ for i in range(my_time,0,-1):
     #sleep for 1 second
     time.sleep(1)
 print("Time's up")
+
